a5d01b9055889b69ba9a2e1771dbf9fd63e16db9
Ah-Rabah <106888788+Ah-Rabah@users.noreply.github.com>
2022-09-28 12:06:16 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    return {key: value * 2 for key, value in my_dict.items()}
+def best_score(my_dict):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my_dict[max]:
+                max = key
+        return max
+    return None
